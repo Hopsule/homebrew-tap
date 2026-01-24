915a30a8f9f510eea267697d40f73abbfd13c2ab
Cagangedik <cagan.gedik@bilgiedu.net>
2026-01-24 18:40:18 +0300

    Fix ARM64 SHA256 checksum
    - ARM64 SHA256: 104307bf113a1b228338810aa9892073bc2cda65724f39f5a18911e24ff4ddaa
    - Intel SHA256: 0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5

--- Formula/hopsule.rb
+++ Formula/hopsule.rb
@@ -7,7 +7,7 @@ class Hopsule < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/Hopsule/cli-tool/releases/download/v0.5.0/decision-darwin-arm64.tar.gz"
-      sha256 "d829041131f795c57a43de41f46c959f6296a26ae743aab2d45424e6cbf0674c"
+      sha256 "104307bf113a1b228338810aa9892073bc2cda65724f39f5a18911e24ff4ddaa"
     elsif Hardware::CPU.intel?
       url "https://github.com/Hopsule/cli-tool/releases/download/v0.5.0/decision-darwin-amd64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
