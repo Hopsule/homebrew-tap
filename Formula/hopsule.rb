85019677544f28d86a5aa01a330559b74eb73149
Cagangedik <cagan.gedik@bilgiedu.net>
2026-01-24 18:46:18 +0300

    Fix: Use on_macos blocks to prevent source build requirement
    - Separate install blocks for ARM64 and Intel
    - No Xcode/build tools required
    - Pre-compiled binaries only

--- Formula/hopsule.rb
+++ Formula/hopsule.rb
@@ -3,22 +3,24 @@ class Hopsule < Formula
   homepage "https://github.com/Hopsule/cli-tool"
   version "0.6.0"
   license "MIT"
-  
-  if OS.mac?
+
+  on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Hopsule/cli-tool/releases/download/v0.6.0/decision-darwin-arm64.tar.gz"
       sha256 "58acf7c38a2d1f1979f468c2a2af134a75ef84cc89d5847291e82b0242ff67db"
-    elsif Hardware::CPU.intel?
+
+      def install
+        bin.install "decision-darwin-arm64" => "hopsule"
+      end
+    end
+
+    if Hardware::CPU.intel?
       url "https://github.com/Hopsule/cli-tool/releases/download/v0.6.0/decision-darwin-amd64.tar.gz"
       sha256 "ab11887544fcf0893b8a0f92e37a26f5e9a8fff2342c1a7879907669d71f8138"
-    end
-  end
 
-  def install
-    if Hardware::CPU.arm?
-      bin.install "decision-darwin-arm64" => "hopsule"
-    elsif Hardware::CPU.intel?
-      bin.install "decision-darwin-amd64" => "hopsule"
+      def install
+        bin.install "decision-darwin-amd64" => "hopsule"
+      end
     end
   end
 
