e98a289f1ccf1d9f8b4ee8dac7e68a9a34415e97
Cagangedik <cagan.gedik@bilgiedu.net>
2026-01-24 20:01:00 +0300

    Release v0.7.4 - Logo Update 🎨

--- Formula/hopsule.rb
+++ Formula/hopsule.rb
@@ -1,21 +1,21 @@
 class Hopsule < Formula
   desc "Decision & Memory Layer for AI teams"
   homepage "https://github.com/Hopsule/cli-tool"
-  url "https://github.com/Hopsule/cli-tool/archive/refs/tags/v0.7.3.tar.gz"
-  sha256 "bd24bf4c9e7e72797e9742ac97e977c7c77eb27274bf729ed29cc247496ef9fa"
+  url "https://github.com/Hopsule/cli-tool/archive/refs/tags/v0.7.4.tar.gz"
+  sha256 "3277e3fb774da277494f92bc49692dd7767da676c72ce22e72f44c4e7ab9743c"
   license "MIT"
   head "https://github.com/Hopsule/cli-tool.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/Hopsule/cli-tool/releases/download/v0.7.3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ae560c0f544ac6ee488c1c9473780432005dcb044942e9f79c748c96382894d5"
-    sha256 cellar: :any_skip_relocation, monterey:       "b5dedb94ec6b9be58cd44f2bbbee0b1d1c080e09f092c557c95b8f1b8b56e7c2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ae560c0f544ac6ee488c1c9473780432005dcb044942e9f79c748c96382894d5"
-    sha256 cellar: :any_skip_relocation, ventura:        "a3e868c93357196803e84c517cf6bf25625b89479b85f2b0ed2d2202b2af8b0e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ae560c0f544ac6ee488c1c9473780432005dcb044942e9f79c748c96382894d5"
-    sha256 cellar: :any_skip_relocation, sonoma:         "2595986831dde17dd01ef309be5d3925628e9249965e0796c7848de1fd39101f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "ae560c0f544ac6ee488c1c9473780432005dcb044942e9f79c748c96382894d5"
-    sha256 cellar: :any_skip_relocation, sequoia:        "a3e868c93357196803e84c517cf6bf25625b89479b85f2b0ed2d2202b2af8b0e"
+    root_url "https://github.com/Hopsule/cli-tool/releases/download/v0.7.4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2709bf7d6d5a51423758b753dd33d57c15425c6c93945360ca1de5d026b2f289"
+    sha256 cellar: :any_skip_relocation, monterey:       "a55c0f65fd7f9bdfe80349c7faf3e790cb40b1dc1856366928f9588b1be883f0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2709bf7d6d5a51423758b753dd33d57c15425c6c93945360ca1de5d026b2f289"
+    sha256 cellar: :any_skip_relocation, ventura:        "a55c0f65fd7f9bdfe80349c7faf3e790cb40b1dc1856366928f9588b1be883f0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2709bf7d6d5a51423758b753dd33d57c15425c6c93945360ca1de5d026b2f289"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a55c0f65fd7f9bdfe80349c7faf3e790cb40b1dc1856366928f9588b1be883f0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "2709bf7d6d5a51423758b753dd33d57c15425c6c93945360ca1de5d026b2f289"
+    sha256 cellar: :any_skip_relocation, sequoia:        "a55c0f65fd7f9bdfe80349c7faf3e790cb40b1dc1856366928f9588b1be883f0"
   end
 
   depends_on "go" => :build
